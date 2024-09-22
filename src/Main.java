a2dc9e1682ddece20109c943394ed166cf435edd
redukee-hse <daartemev@edu.hse.ru>
2024-09-22 16:57:14 +0300

    Добавил условие на ввод чисел

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git!!!");
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Они не должны быть равны 0 !!!");
     }
 }
