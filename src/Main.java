f5d4da40227438e5d38d7266b2e4acd5966415d2
redukee-hse <daartemev@edu.hse.ru>
2024-09-22 16:49:23 +0300

    Проинициализировал 2 введенных числа

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git!!!");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите 2 числа:");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
 
     }
 }
